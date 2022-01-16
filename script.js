31e83ea54bbb81f3ed0295076da4cb398297e770
Jonamart <54645868+Jonamarti@users.noreply.github.com>
2022-01-16 21:55:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+window.onload = function(){
+
+
+	document.getElementById("addNewTask").addEventListener("click", addTask);
+	var id = 0;
+	
+	
+	/*
+	function deleteTask(id){
+		alert(id);
+		//document.getElementById(id).remove();
+		return 1;
+	}
+
+	deleteTask = (id) =>{
+		alert(id);
+		//document.getElementById(id).remove();
+		return 1;
+	}
+	*/
+
+	function addTask(deleteTask){
+		
+		
+		const newTask = `<h2 id=${id}> <p>Heyy this is the task number ${id} ! 
+		<button class="deleteButt" 
+		
+		onClick="{deleteTask(${id})}"
+		
+		>
+		Delete this task
+		</button>
+		</p> 
+		</h2>`;
+		id+=1;
+		
+		document.getElementById("tasks_div").innerHTML+=newTask;
+		return;
+	}
+
+
+	// TODO 
+	/*
+	const fetchData = async(username) =>{
+
+		let response = await fetch(`https://api.github.com/users/${username}`);
+		console.log(response);
+		let json = await response.json();
+		console.log(json);
+	}
+	fetchData("Jonamarti");
+	*/
+
+}
