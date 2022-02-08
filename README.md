97ea9b9076596b59f1e22f00ac85e2dac0b8c393
Jonamart <54645868+Jonamarti@users.noreply.github.com>
2022-02-08 11:44:36 +0100

    CRUD without save

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # TodoAppPureJS
+
+Simple to do app with vanilla JS.
+100% client side, so loads and stores (TO FIX) tasks via a .json file.
+
+
+TODO:
+fix store json file.
+refactor functions
--- script.js
+++ script.js
@@ -1,53 +1,223 @@
 window.onload = function () {
 
-
-	//document.getElementById("addNewTask").addEventListener("click", addTask);
-	var id = 0;
-
-
-	/*
-	function deleteTask(id){
-		alert(id);
-		//document.getElementById(id).remove();
-		return 1;
+	if (localStorage["tasks"] !== undefined) {
+		displayJSONtasks();
+		document.getElementById("loadTasksDiv").style.display = 'none';
 	}
 
-	deleteTask = (id) =>{
-		alert(id);
-		//document.getElementById(id).remove();
-		return 1;
+}
+
+async function addTask() {
+
+	console.log("addtask")
+	var taskNumber = 0;
+
+	try {
+		if (localStorage["tasks"] !== undefined) {
+
+
+			taskNumber = await JSON.parse(localStorage["tasks"]).at(-1)["id"];
+		}
+	} catch (TypeError) {
+		console.log("Type error");
+		taskNumber = -1;
 	}
-	*/
 
-	// function addTask() {
-	// 	alert("caling addtask")
-	// 	let new_task_input = document.getElementById("new_task_input").value;
-	// 	alert(new_task_input);
-	// 	let newTask = `<h2 id=${id}> <p>Heyy this is the task number ${id} ! 
-	// 	<button class="deleteButt" 
-	// 	onClick="{deleteTask(${id})}"
-	// 	>
-	// 	Delete this task
-	// 	</button>
-	// 	</p> 
-	// 	</h2>`;
-	// 	id += 1;
-
-	// 	document.getElementById("tasks_div").innerHTML += newTask;
-	// 	return;
-	// }
+	// TODO refactor error -> if empty, task number = 0
 
 
-	// TODO 
+	title = document.getElementById("Task_title").value;
+	init_date = document.getElementById("Task_init_date").value;
+	end_date = document.getElementById("Task_end_date").value;
+	tags = document.getElementById("Task_tags").value;
+	description = document.getElementById("Task_description").value;
 
-	// const fetchData = async(username) =>{
+	new_task = {
+		"id": taskNumber + 1,
+		"title": title,
+		"init_date": init_date,
+		"end_date": end_date,
+		"description": description,
+		"attachments": {}
+	}
+	saveTaskToLocalStorage(new_task);
 
-	// 	let response = await fetch(`https://api.github.com/users/${username}`);
-	// 	console.log(response);
-	// 	let json = await response.json();
-	// 	console.log(json);
-	// }
-	// fetchData("Jonamarti");
+	event.currentTarget.submit();
+
+}
+
+async function showPlaceholderUpdate(id){
+	let tasks = fromlocalStorageJSONtasks();
+
+	// use placeholder
+	
+	title = tasks[id].title;
+	document.getElementById("update_Task_title").placeholder=title;
+
+	description = tasks[id].description;
+	document.getElementById("update_Task_description").placeholder=description;
+
+	tags = tasks[id].tags;
+	document.getElementById("update_Task_tags").placeholder=tags;
 
 
+	document.getElementById('updateTaskForm').setAttribute('data-id',id);
+}
+
+
+async function updateTask(id) {
+
+	let tasks = fromlocalStorageJSONtasks();
+
+	// to update 
+
+	title = document.getElementById("update_Task_title").value;
+	init_date = document.getElementById("update_Task_init_date").value;
+	end_date = document.getElementById("update_Task_end_date").value;
+	tags = document.getElementById("update_Task_tags").value;
+	description = document.getElementById("update_Task_description").value;
+
+
+	updated_task = {
+		"id": id,
+		"title": title,
+		"init_date": init_date,
+		"end_date": end_date,
+		"description": description,
+		"attachments": {}
+	}
+
+	saveTaskToLocalStorage(updated_task);
+
+	event.currentTarget.submit();
+
+}
+
+
+
+
+async function saveTaskToLocalStorage(task) {
+
+	let tasks = JSON.parse(localStorage.getItem("tasks"));
+
+	tasks[task.id] = task;
+
+	localStorage.setItem(
+		"tasks", JSON.stringify(tasks)
+	)
+
+}
+
+function deleteTask(id) {
+	let tasks = JSON.parse(localStorage.getItem("tasks"));
+	tasks.splice(id, id - 1);
+	localStorage.setItem("tasks", JSON.stringify(tasks));
+	displayJSONtasks();
+
+}
+
+
+async function displayJSONtasks() {
+	if (localStorage["tasks"] !== undefined) {
+		try {
+			let taskArray = await fromlocalStorageJSONtasks();
+			let divTasks = document.getElementById("new_task_div");
+			divTasks.innerHTML = "";
+			taskArray.forEach((task) => {
+				divTasks.innerHTML += `<div class="task" data-id="${task["id"]}" id=task_${task["id"]}>
+	<p >
+		<b> ${task["title"]}</b><br> ` +
+
+					(task["end_date"] == "" ?
+						`${task["init_date"]}` :
+						`${task["init_date"]} - ${task["end_date"]}`) // if end date null, just init date
+
+					+ `<br>
+		
+		${task["description"]}
+	</p>
+	<button onclick="deleteTask(${task["id"]})">X</button>
+	<button onclick="{document.getElementById('updateTaskDiv').style.display='block';
+	showPlaceholderUpdate(${task["id"]});
+	
+}">Edit</button>
+</div > `
+			});
+			return 1;
+		} catch (error) {
+			console.log(error);
+			return 0;
+		}
+	}
+	else {
+		await tolocalStorageJSONtasks();
+		try {
+			let taskArray = await fromlocalStorageJSONtasks();
+			let divTasks = document.getElementById("new_task_div");
+			taskArray.forEach((task) => {
+				divTasks.innerHTML += `<div class="task" id=task_${task["id"]}>
+	<p >
+		<b> ${task["title"]}</b><br> ` +
+
+					(task["end_date"] == "" ?
+						`${task["init_date"]}` :
+						`${task["init_date"]} - ${task["end_date"]}`) // if end date null, just init date
+
+					+ `<br>
+		
+		${task["description"]}
+	</p>
+	<button onclick="deleteTask(${task["id"]})">X</button><button>Edit</button>
+</div > `
+			});
+			return 1;
+		} catch (error) {
+			console.log(error);
+			return 0;
+		}
+
+	}
+}
+
+function sidebar_open() {
+	document.getElementById("mySidebar").style.display = "block";
+	document.getElementById("myOverlay").style.display = "block";
+}
+
+function sidebar_close() {
+	document.getElementById("mySidebar").style.display = "none";
+	document.getElementById("myOverlay").style.display = "none";
+}
+
+async function getJSONtasksFromFile() {
+	var loadInp = document.getElementById("loadTasksInput");
+	return loadInp.files[0].text().then(
+		response => {
+			return JSON.parse(response);
+		}
+	)
+}
+
+
+/**
+		 * @param  object result
+		 * This method saves json tasks in local storage
+		 * metadata is erased when parsing it 
+		 * 
+		 * */
+async function tolocalStorageJSONtasks() {
+	let result = await getJSONtasksFromFile();
+	let tasks = result["tasks"];
+
+	localStorage.setItem("tasks", JSON.stringify(tasks))
+
+}
+
+/**
+		 * This method loads json tasks from local storage
+		 * returns: Array with task objects inside
+		 * */
+function fromlocalStorageJSONtasks() {
+	let tasks = JSON.parse(localStorage.getItem("tasks"));
+	return tasks;
 }
