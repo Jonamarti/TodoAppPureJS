925178e1708bca81bfd3bc3bb05ed81a0e4a0e54
Jonamarti <thisisparto@gmail.com>
2022-02-19 13:54:08 +0100

    Reset add task, css fixes

--- script.js
+++ script.js
@@ -6,6 +6,54 @@ window.onload = function () {
 	}
 
 
+
+	// /* Draggable div */
+
+	// // Make the DIV element draggable:
+
+	// // document.getElementById("addTaskDiv").addEventListener(onmousedown, console.log("onmousedown"));
+	// // document.querySelector("#addTaskDiv").addEventListener(onmousedown, console.log("onmousedown"));
+	// var drag = document.querySelector("#addTaskDiv form fieldset legend");
+	
+	// // dragElement(document.getElementById("addTaskDiv"));
+	// dragElement(drag);
+
+	// function dragElement(elmnt) {
+	// 	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+	// 	elmnt.onmousedown = dragMouseDown;
+
+
+	// 	function dragMouseDown(e) {
+	// 		e = e || window.event;
+	// 		e.preventDefault();
+	// 		// get the mouse cursor position at startup:
+	// 		pos3 = e.clientX;
+	// 		pos4 = e.clientY;
+	// 		document.onmouseup = closeDragElement;
+	// 		// call a function whenever the cursor moves:
+	// 		document.onmousemove = elementDrag;
+	// 	}
+
+	// 	function elementDrag(e) {
+	// 		e = e || window.event;
+	// 		e.preventDefault();
+	// 		// calculate the new cursor position:
+	// 		pos1 = pos3 - e.clientX;
+	// 		pos2 = pos4 - e.clientY;
+	// 		pos3 = e.clientX;
+	// 		pos4 = e.clientY;
+	// 		// set the element's new position:
+	// 		elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+	// 		elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+	// 	}
+
+	// 	function closeDragElement() {
+	// 		// stop moving when mouse button is released:
+	// 		document.onmouseup = null;
+	// 		document.onmousemove = null;
+	// 	}
+	// }
+
 }
 
 async function addTask() {
@@ -242,10 +290,11 @@ const download = (path, filename) => {
 
 };
 
-function parseToDownload(){
+function parseToDownload() {
 	jsonData = new Object({ "tasks": JSON.parse(localStorage["tasks"]) });
 	const blob = new Blob([JSON.stringify(jsonData)], { type: 'application/json' });
 
 	const url = URL.createObjectURL(blob);
 	download(url, "tasksdownloaded.json");
 }
+
